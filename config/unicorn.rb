a95b1eac775a6d25015700f4af14d247c293f43a
Pietro <pietro.passarelli@gmail.com>
2015-10-12 10:44:03 +0100

    unicorn.rb - for heroku ffmpeg

---
+++ config/unicorn.rb
@@ -0,0 +1,26 @@
+# config/unicorn.rb
+# https://devcenter.heroku.com/articles/rails-unicorn
+
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+#timeout 15 reccomended
+timeout 120 
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
